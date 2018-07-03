2e083cad00fc0e1cabd3aba2fe466033308bf8df
unknown <daniel.ungersboeck@gmail.com>
2018-07-03 22:56:28 +0200

    change AppContent to static component with routes to content

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,39 +1,24 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
+import { Switch, Route } from 'react-router-dom';
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Home from './Home';
 
 // import strings from '../localization/game-locale';
 
 import Navigation from './Navigation';
 
-const styles = () => ({
-  grid: {},
-});
-
 class AppContent extends Component {
   render() {
-    const { classes } = this.props;
-
     return (
       <Navigation>
-        <Grid container className={classes.grid} justify="center">
-          <Grid item xs={12}>
-            <Typography variant="display3" gutterBottom>
-              Business Website
-            </Typography>
-          </Grid>
-        </Grid>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/projects" component={Projects} /> */}
+        </Switch>
       </Navigation>
     );
   }
 }
 
-AppContent.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(AppContent);
+export default AppContent;
